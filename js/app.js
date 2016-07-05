7bef15ee4b88d5049acfded9b1c9b3f47181c4c6
rasheedgm <gm.abdul@gmail.com>
2016-07-05 14:24:06 +0530

    First Test
    test

--- js/app.js
+++ js/app.js
@@ -1,20 +1,19 @@
 app = angular.module('stockMng', ["firebase"]);
 app.controller('appCtrl', ["$scope", "$firebaseObject",function($scope,$firebaseObject) {
     // Initialize Firebase
-    //var ref = new Firebase("https://dbstock.firebaseio.com/");
+    //var ref = new Firebase("https://neralu-99f71.firebaseio.com");
     var ref = firebase.database().ref();  
-    var membObject = $firebaseObject(ref.child("members"));   
-    var projObject = $firebaseObject(ref.child("projects"));   
-    var accountObject =$firebaseObject(ref.child("accounts"));
-    $scope.members = membObject;
-    $scope.projects = projObject;
-    $scope.accounts = accountObject;
+    $scope.members = $firebaseObject(ref.child("members"));   
+    $scope.projects = $firebaseObject(ref.child("projects"));   
+    $scope.accounts =$firebaseObject(ref.child("accounts"));  
+    $scope.zones =$firebaseObject(ref.child("zones")); 
+    $scope.show={};
+    $scope.show.Details=false;
     $scope.user={};
     $scope.user.auth =function(){ 
         firebase.auth().signInWithEmailAndPassword($scope.user.email, $scope.user.password)
             .then(function(userData){
             console.log("Loged In As: "+ userData.email);
-            userAuthenticated();
         })
             .catch(function(error) {
             // Handle Errors here.
@@ -28,9 +27,11 @@ app.controller('appCtrl', ["$scope", "$firebaseObject",function($scope,$firebase
         if (user) {
             // User is signed in.
             $scope.user.authenticated =true;
+            $scope.$apply();
         } else {
             // No user is signed in.
             $scope.user.authenticated =false;
+            $scope.$apply();
         }
     });
     $scope.user.unauth=function(){   
@@ -38,9 +39,41 @@ app.controller('appCtrl', ["$scope", "$firebaseObject",function($scope,$firebase
         }, function(error) {
         });
     }
-    function userAuthenticated(){
-        $scope.user.authenticated =true;
+    
+    $scope.getProjectTotalPromised = function(projId){
+        var totalPromise =0;
+        angular.forEach($scope.accounts, function(account,key){
+            if(account.Project==projId){
+                totalPromise=totalPromise+account.AmountPromised;
+            }
+        });
+        return(totalPromise);
+        
+        
     }
+    $scope.getProjectTotalPaid = function(projId){
+        var totalPaid =0;
+        angular.forEach($scope.accounts, function(account,key){
+            if(account.Project==projId){
+                totalPaid=totalPaid+account.AmountPaid;
+            }
+        });
+        return(totalPaid);
+    }
+    $scope.clickList = function(pId){
+        $scope.show.selectedProject=pId;
+    }
+    $scope.toggleDetails = function(){
+        if ($scope.show.Details){
+            $scope.show.Details=false;
+            $scope.show.$apply;
+        }else{
+            $scope.show.Details=true;            
+            $scope.show.$apply;
+        }
+    }
+    
+    //new member
     $scope.addNewMember = function(){ 
         var tempNewMember={"Id": $scope.newMember.Id,"Name":$scope.newMember.Name,"Phone":$scope.newMember.Phone};
         $scope.members[$scope.newMember.Id]=tempNewMember;  
@@ -75,6 +108,7 @@ app.controller('appCtrl', ["$scope", "$firebaseObject",function($scope,$firebase
             $scope.newProject.Id=projectId;            
             var tempNewProject={"Id" : $scope.newProject.Id,"Name": $scope.newProject.Name,"Details": $scope.newProject.Details,"Contact": $scope.newProject.Contact};
             $scope.projects[tempNewProject.Id]=tempNewProject;
+            //$scope.projects=tempNewProject;
             $scope.projects.$save().then(function(ref) {            
             $scope.newProject={};
                 alert("Updated");
@@ -93,15 +127,7 @@ app.controller('appCtrl', ["$scope", "$firebaseObject",function($scope,$firebase
         var accountId = $scope.accounts.counter;
         accountId =  "ACT"+ pad(accountId);
         $scope.newAccount.Id=accountId;
-        var tempNewAccount={"Id" : $scope.newAccount.Id,
-                            "Date" :$scope.newAccount.Date.getTime(),
-                            "AmountPromised": $scope.newAccount.AmountPromised,
-                            "AmountPaid": ($scope.newAccount.AmountPaid||0),
-                            "Project": $scope.newAccount.Project,
-                            "Representative": $scope.newAccount.Representative,
-                            "Zone": $scope.newAccount.Zone,
-                            "PaymentMode": "Cash"
-                           };
+        var tempNewAccount={"Id" : $scope.newAccount.Id,"Date" :$scope.newAccount.Date.getTime(),"Name":$scope.newAccount.Name,"AmountPromised": ($scope.newAccount.AmountPromised||0),"AmountPaid": ($scope.newAccount.AmountPaid||0),"Project": $scope.newAccount.Project,"Representative": $scope.newAccount.Representative,"Zone": $scope.newAccount.Zone,"PaymentMode": "Cash"};
         $scope.accounts[tempNewAccount.Id]=tempNewAccount;
         $scope.accounts.$save().then(function(ref) {            
             $scope.newAccount={};
@@ -139,5 +165,11 @@ app.directive('member',function(){
     return{
         restrict: 'E',
         templateUrl: 'templates/member.html'
+    }
+});
+app.directive('proj-details',function(){
+    return{
+        restrict: 'E',
+        templateUrl: 'templates/proj-details.html'
     }
 });
